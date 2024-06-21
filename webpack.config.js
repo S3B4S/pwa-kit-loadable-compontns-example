d98015a5d6881f2ec39c438d8c4fb1155d0196c8
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2024-06-21 15:21:13 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {merge} = require('webpack-merge')
+const originalConfigs = require('@salesforce/pwa-kit-dev/configs/webpack/config.js')
+
+const customConfig = {
+    optimization: {
+        minimize: process.env.NODE_ENV === 'production',
+        splitChunks: {
+            maxSize: 1024 * 100 /* KB */
+        }
+    }
+}
+
+const logId = (x) => {
+    console.log(x)
+    return x
+}
+
+// Check if originalConfigs is an array and merge each configuration with customConfig
+if (Array.isArray(originalConfigs)) {
+    module.exports = logId(originalConfigs.map((config) => merge(config, customConfig)))
+} else {
+    module.exports = merge(originalConfigs, customConfig)
+}
